05fb04a4894fde5f9acc353cb904ee747369f4d1
notunderctrl <119873044+notunderctrl@users.noreply.github.com>
2022-12-29 17:24:05 +0300

    Start part 5

---
+++ 05 - Button Roles/src/index.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on('interactionCreate', (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'embed') {
+    const embed = new EmbedBuilder()
+      .setTitle('Embed title')
+      .setDescription('This is an embed description')
+      .setColor('Random')
+      .addFields(
+        {
+          name: 'Field title',
+          value: 'Some random value',
+          inline: true,
+        },
+        {
+          name: '2nd Field title',
+          value: 'Some random value',
+          inline: true,
+        }
+      );
+
+    interaction.reply({ embeds: [embed] });
+  }
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'embed') {
+    const embed = new EmbedBuilder()
+      .setTitle('Embed title')
+      .setDescription('This is an embed description')
+      .setColor('Random')
+      .addFields(
+        {
+          name: 'Field title',
+          value: 'Some random value',
+          inline: true,
+        },
+        {
+          name: '2nd Field title',
+          value: 'Some random value',
+          inline: true,
+        }
+      );
+
+    message.channel.send({ embeds: [embed] });
+  }
+});
+
+client.login(process.env.TOKEN);
