d2a98e3beaa851326be277516d7ce3ff67e658aa
notunderctrl <119873044+notunderctrl@users.noreply.github.com>
2022-12-29 19:27:00 +0300

    Clear index

--- 05 - Button Roles/src/index.js
+++ 05 - Button Roles/src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -12,54 +12,6 @@ const client = new Client({
 
 client.on('ready', (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
-});
-
-client.on('interactionCreate', (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'embed') {
-    const embed = new EmbedBuilder()
-      .setTitle('Embed title')
-      .setDescription('This is an embed description')
-      .setColor('Random')
-      .addFields(
-        {
-          name: 'Field title',
-          value: 'Some random value',
-          inline: true,
-        },
-        {
-          name: '2nd Field title',
-          value: 'Some random value',
-          inline: true,
-        }
-      );
-
-    interaction.reply({ embeds: [embed] });
-  }
-});
-
-client.on('messageCreate', (message) => {
-  if (message.content === 'embed') {
-    const embed = new EmbedBuilder()
-      .setTitle('Embed title')
-      .setDescription('This is an embed description')
-      .setColor('Random')
-      .addFields(
-        {
-          name: 'Field title',
-          value: 'Some random value',
-          inline: true,
-        },
-        {
-          name: '2nd Field title',
-          value: 'Some random value',
-          inline: true,
-        }
-      );
-
-    message.channel.send({ embeds: [embed] });
-  }
 });
 
 client.login(process.env.TOKEN);
