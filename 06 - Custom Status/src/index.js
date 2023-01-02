bd37fd07407d243dfab1ba161eb1bf96864b5525
notunderctrl <underctrl.io@gmail.com>
2023-01-02 12:06:52 +0300

    remove interactionCreate event listener

--- 06 - Custom Status/src/index.js
+++ 06 - Custom Status/src/index.js
@@ -14,32 +14,4 @@ client.on('ready', (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-  try {
-    if (!interaction.isButton()) return;
-    await interaction.deferReply({ ephemeral: true });
-
-    const role = interaction.guild.roles.cache.get(interaction.customId);
-    if (!role) {
-      interaction.editReply({
-        content: "I couldn't find that role",
-      });
-      return;
-    }
-
-    const hasRole = interaction.member.roles.cache.has(role.id);
-
-    if (hasRole) {
-      await interaction.member.roles.remove(role);
-      await interaction.editReply(`The role ${role} has been removed.`);
-      return;
-    }
-
-    await interaction.member.roles.add(role);
-    await interaction.editReply(`The role ${role} has been added.`);
-  } catch (error) {
-    console.log(error);
-  }
-});
-
 client.login(process.env.TOKEN);
