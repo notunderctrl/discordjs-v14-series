79a43d2b136a6076e73bc988d8775c5fa047a8de
notunderctrl <underctrl.io@gmail.com>
2023-01-03 20:04:44 +0300

    Start part 7

---
+++ 07 - Command Handler/src/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Client, IntentsBitField, ActivityType } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+let status = [
+  {
+    name: 'Under Ctrl',
+    type: ActivityType.Streaming,
+    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+  },
+  {
+    name: 'Custom Status 1',
+  },
+  {
+    name: 'Custom Status 2',
+    type: ActivityType.Watching,
+  },
+  {
+    name: 'Custom Status 3',
+    type: ActivityType.Listening,
+  },
+];
+
+client.on('ready', (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+
+  setInterval(() => {
+    let random = Math.floor(Math.random() * status.length);
+    client.user.setActivity(status[random]);
+  }, 10000);
+});
+
+client.login(process.env.TOKEN);
