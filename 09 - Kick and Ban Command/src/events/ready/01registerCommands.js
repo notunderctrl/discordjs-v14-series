e4e65e5b8e5b7783f9f0e2c2ae2dc63de8463787
notunderctrl <underctrl.io@gmail.com>
2023-01-18 00:23:44 +0300

    Fix typo

--- 09 - Kick and Ban Command/src/events/ready/01registerCommands.js
+++ 09 - Kick and Ban Command/src/events/ready/01registerCommands.js
@@ -51,6 +51,6 @@ module.exports = async (client) => {
       }
     }
   } catch (error) {
-    console.log(`TThere was an error: ${error}`);
+    console.log(`There was an error: ${error}`);
   }
 };
