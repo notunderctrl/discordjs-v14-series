d055430814ca8e4db50b929b785c473d5a7a3d3e
notunderctrl <underctrl.io@gmail.com>
2023-01-17 17:29:27 +0300

    Start part 9

---
+++ 09 - Kick and Ban Command/src/events/ready/01registerCommands.js
@@ -0,0 +1,56 @@
+const { testServer } = require('../../../config.json');
+const areCommandsDifferent = require('../../utils/areCommandsDifferent');
+const getApplicationCommands = require('../../utils/getApplicationCommands');
+const getLocalCommands = require('../../utils/getLocalCommands');
+
+module.exports = async (client) => {
+  try {
+    const localCommands = getLocalCommands();
+    const applicationCommands = await getApplicationCommands(
+      client,
+      testServer
+    );
+
+    for (const localCommand of localCommands) {
+      const { name, description, options } = localCommand;
+
+      const existingCommand = await applicationCommands.cache.find(
+        (cmd) => cmd.name === name
+      );
+
+      if (existingCommand) {
+        if (localCommand.deleted) {
+          await applicationCommands.delete(existingCommand.id);
+          console.log(`🗑 Deleted command "${name}".`);
+          continue;
+        }
+
+        if (areCommandsDifferent(existingCommand, localCommand)) {
+          await applicationCommands.edit(existingCommand.id, {
+            description,
+            options,
+          });
+
+          console.log(`🔁 Edited command "${name}".`);
+        }
+      } else {
+        if (localCommand.deleted) {
+          console.log(
+            `⏩ Skipping registering command "${name}" as it's set to delete.`
+          );
+          continue;
+        }
+
+        await applicationCommands.create({
+          name,
+          description,
+          options,
+        });
+
+        console.log(`👍 Registered command "${name}."`);
+      }
+    }
+  } catch (error) {
+    console.log(`TThere was an error: ${error}`);
+  }
+};
---
+++ 09 - Kick and Ban Command/src/events/ready/consoleLog.js
@@ -0,0 +1,3 @@
+module.exports = (client) => {
+  console.log(`${client.user.tag} is online.`);
+};
