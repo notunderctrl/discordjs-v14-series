01755891e3c761d7cc300eb0e306126a163b9cef
notunderctrl <underctrl.io@gmail.com>
2023-01-30 14:36:41 +0300

    Complete part 11

--- 11 - Connect to DB/src/index.js
+++ 11 - Connect to DB/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
@@ -11,6 +12,16 @@ const client = new Client({
   ],
 });
 
-eventHandler(client);
+(async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI, { keepAlive: true });
+    console.log('Connected to DB.');
+
+    eventHandler(client);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
 
 client.login(process.env.TOKEN);
