7d00acf8e3862d9e3453acaee9ebc3cbf09042d2
notunderctrl <underctrl.io@gmail.com>
2023-02-02 13:27:47 +0300

    Start part 12

---
+++ 12 - Levelling system/src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+const mongoose = require('mongoose');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+(async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGODB_URI, { keepAlive: true });
+    console.log('Connected to DB.');
+
+    eventHandler(client);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+})();
+
+client.login(process.env.TOKEN);
