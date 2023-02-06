98bc94b74ff8ae06c5a7edd116f1fba2d863694d
notunderctrl <underctrl.io@gmail.com>
2023-02-06 14:29:22 +0300

    Complete part 13

--- 13 - Level command/src/index.js
+++ 13 - Level command/src/index.js
@@ -8,6 +8,7 @@ const client = new Client({
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildPresences,
     IntentsBitField.Flags.MessageContent,
   ],
 });
